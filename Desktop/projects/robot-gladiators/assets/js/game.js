4442b46fa85814db81841edf86cb779907211fbe
blanebussey <blanebussey@gmail.com>
2021-09-27 15:04:05 -0500

    js windows

--- Desktop/projects/robot-gladiators/assets/js/game.js
+++ Desktop/projects/robot-gladiators/assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an Alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName)
+console.log("This logs a string, good for leaving yourself a message")
+//this will do math and log 20
+console.log(10 + 10);
+//what is this?
+console.log("Our Robot's name is" + playerName)
+
+//this create a function named "fight" - also notice this is how you takes notes
+function fight() {
+    window.alert("The fight has begun!");
+}
+fight();
+// fight();
